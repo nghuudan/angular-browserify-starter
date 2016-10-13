c6c8b127475ae09cc46868baadd2e88e31b78689
Dan Nguyen <nghuudan@gmail.com>
2016-10-13 10:19:22 -0400

    Add karma-browserify test preprocessor

--- karma.conf.js
+++ karma.conf.js
@@ -3,13 +3,20 @@ module.exports = function(config) {
 
 	config.set({
 		basePath: './',
+		browserify: {
+			debug: true,
+			transform: ['babelify']
+		},
 		browsers: ['PhantomJS'],
 		files: [
-			'build/**/*.js',
+			'src/**/*.js',
 			'node_modules/angular-mocks/angular-mocks.js',
 			'test/**/*.spec.js'
 		],
-		frameworks: ['jasmine'],
+		frameworks: ['browserify', 'jasmine'],
+		preprocessors: {
+			'src/**/*.js': ['browserify']
+		},
 		reporters: ['spec'],
 		singleRun: true
 	});
